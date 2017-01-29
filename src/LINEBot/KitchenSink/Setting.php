6d3e4dc9fc6e4d8c8aa1ffe60a86ae65102dcb32
mikung <mikung555@gmail.com>
2017-01-29 22:05:57 +0700

    Update Setting.php

--- src/LINEBot/KitchenSink/Setting.php
+++ src/LINEBot/KitchenSink/Setting.php
@@ -33,8 +33,7 @@ class Setting
 
                 'bot' => [
                     'channelToken' => getenv('LINEBOT_CHANNEL_TOKEN') ?: 'DqC0E6bwB9GJjUsZdCdyhtC1b6KXlp/DXoBnzPbt1/v+z0p0FCzHk5XAbO9nm2HQL8AoawkXmbJmMabvXfKrdJeueUzpp27IKe8kDox3Y4U2hjOjsM2l32hIc47h7TrPHrhtfeRQyXLwALD3yP1EJAdB04t89/1O/w1cDnyilFU=',
-                    'channelSecret' => getenv('LINEBOT_CHANNEL_SECRET') ?: 'ec68c63b493064bedb0c8eccf7f328d6
-',
+                    'channelSecret' => getenv('LINEBOT_CHANNEL_SECRET') ?: 'ec68c63b493064bedb0c8eccf7f328d6',
                 ],
 
                 'apiEndpointBase' => getenv('LINEBOT_API_ENDPOINT_BASE'),
